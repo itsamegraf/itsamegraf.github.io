5c97f54f5a544b4f574bf6430b1f7464cada59ee
Daniel Graf <59334873+itsamegraf@users.noreply.github.com>
2024-11-24 10:08:52 +0100

    Revert "#12 rotation bug fix (#13)"
    This reverts commit 6f85103858197f109bb181208d65cbf643c046a8.

--- bin/gsap/init.js
+++
@@ -1,7 +0,0 @@
-import { gsap } from '@gsap-core';
-import { PixiPlugin } from '@gsap-pixi';
-import { MotionPathPlugin } from '@gsap-motion-path';
-import { EasePack } from "@gsap-ease";
-gsap.registerPlugin(PixiPlugin, MotionPathPlugin, EasePack);
-
-export {gsap, PixiPlugin};
